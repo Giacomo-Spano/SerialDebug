ff17b2268c168db90a676ae400ff6c03f7bac948
GIACOMO SPANO <giacomo.spano@libero.it>
2018-07-29 07:29:43 +0200

    Add project files.

---
+++ SerialDebug/SerialDebug.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		SerialDebug.ino
+ Created:	7/29/2018 7:29:34 AM
+ Author:	giaco
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
